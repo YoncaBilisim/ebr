5ec4626686d9a7697750063b91ac3e7717e92249
Muhammed YÜRÜRDURMAZ <yururdurmaz.muhammed@yoncabt.com.tr>
2016-01-13 14:02:09 +0200

    ilk hali

---
+++ src/main/java/com/yoncabt/abys/ReportServerApplication.java
@@ -0,0 +1,12 @@
+package com.yoncabt.abys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportServerApplication.class, args);
+    }
+}
