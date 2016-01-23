2f88bd752bc54ac29c726ef73b29aad808e25811
Muhammed YÜRÜRDURMAZ <yururdurmaz.muhammed@yoncabt.com.tr>
2016-01-23 21:55:35 +0200

    rapor istek formatı düzenleniyor

--- src/main/java/com/yoncabt/abys/report/ReportRequest.java
+++ src/main/java/com/yoncabt/abys/report/ReportRequest.java
@@ -5,10 +5,38 @@
  */
 package com.yoncabt.abys.report;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- *
+ * rapor isteği için request sınıfı
  * @author myururdurmaz
  */
 public class ReportRequest {
 
+    private Map<String, Object> reportParams = new HashMap<>();
+    /**
+     * eğer <code>true</code> ise asenkron çalışacakltır. değilse hemen çıktı üretecektir
+     */
+    private boolean async;
+    /**
+     * eğer emaila dresi varsa bitince mail atılabilir
+     */
+    private String email;
+    /**
+     * zamanlanmış rapor için kullanılacak
+     */
+    private long scheduleTime;
+    /**
+     * bunu birden fazla veritabanında çalıştırabilmek için kullanqcağım.<br />
+     * eğer verilmezse default isimli kullanılsın<br />
+     * <br />
+     * abys.conf içerisinde<br />
+     * <pre>    report.datasource.datasourceName.url=veritabanı urlsi</pre>
+     * <pre>    report.datasource.datasourceName.user=veritabanı kullanıcı</pre>
+     * <pre>    report.datasource.datasourceName.pass=veritabanı şifre</pre>
+     * hatta farklı veritabanları da kullanılabilsin
+     * <pre>    report.datasource.datasourceName.driver=jdbc driver</pre>
+     */
+    private String datasourceName;
 }
