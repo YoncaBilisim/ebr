3131ac6867c39fbb4e3ead65a29206a54209bccd
Muhammed YÜRÜRDURMAZ <yururdurmaz.muhammed@yoncabt.com.tr>
2016-02-02 14:47:51 +0200

    versiyon dosyası değişikliği

--- src/main/java/com/yoncabt/ebr/util/VersionUtil.java
+++ src/main/java/com/yoncabt/ebr/util/VersionUtil.java
@@ -14,9 +14,9 @@ import java.util.Date;
  */
 public class VersionUtil {
 
-    public static final long WAR_DATE = 1000L * 1452346176;
+    public static final long WAR_DATE = 1000L * 1454417271;
     public static final Date START_DATE = new Date();
-    public static final String GIT_ID = "06da011c97e51f836a2865420a905f3cd8eb8642";
+    public static final String GIT_ID = "ba2cc9af52c58dcc264759d7336319ffd516a0b5";
 
     public static void print() {
         System.out.println("WAR_DATE :" + new Date(WAR_DATE));
